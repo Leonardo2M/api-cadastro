39735e16ae21a154fbd78b86fd1f0edcb013e3d1
Leonardo2M <leonardomm.contato@gmail.com>
2023-07-11 21:52:27 -0300

    feat: endpoint POST in PessoaController

--- src/main/java/br/com/test/api/controller/PessoaController.java
+++ src/main/java/br/com/test/api/controller/PessoaController.java
@@ -1,9 +1,14 @@
 package br.com.test.api.controller;
 
+import br.com.test.api.domain.service.email.EmailService;
 import br.com.test.api.domain.service.usuario.PessoaService;
 import br.com.test.api.dto.pessoa.PessoaAniversarioDTO;
+import br.com.test.api.dto.email.MessagemParabens;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,13 +17,21 @@ import java.util.List;
 public class PessoaController {
 
     private final PessoaService pessoaService;
+    private final EmailService emailService;
 
-    public PessoaController(PessoaService pessoaService) {
+    public PessoaController(PessoaService pessoaService, EmailService emailService) {
         this.pessoaService = pessoaService;
+        this.emailService = emailService;
     }
 
     @GetMapping("/aniversarios")
     public ResponseEntity<List<PessoaAniversarioDTO>> consultarAniversarioDaSeamana() {
         return pessoaService.consultarAniversariantes();
     }
+
+    @PostMapping("/aniversarios")
+    public ResponseEntity<List<PessoaAniversarioDTO>> enviarParabens(@Valid @RequestBody MessagemParabens messagemParabens) {
+        return emailService.enviarParabens(messagemParabens);
+    }
+
 }
--- src/main/java/br/com/test/api/domain/service/email/EmailService.java
+++ src/main/java/br/com/test/api/domain/service/email/EmailService.java
@@ -4,14 +4,18 @@ import br.com.test.api.domain.model.EmailModel;
 import br.com.test.api.domain.model.enums.StatusEmail;
 import br.com.test.api.domain.repository.email.EmailRepository;
 import br.com.test.api.dto.email.EmailDTO;
+import br.com.test.api.dto.pessoa.PessoaAniversarioDTO;
+import br.com.test.api.dto.email.MessagemParabens;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -68,10 +72,28 @@ public class EmailService {
         }
     }
 
-    public static String gerarSenha() {
+    public ResponseEntity<List<PessoaAniversarioDTO>> enviarParabens(MessagemParabens messagemParabens) {
+        try{
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom(this.emailFrom);
+            message.setTo(messagemParabens.getToEmail());
+            message.setSubject(messagemParabens.getNome() + " te deseja parabéns!");
+            message.setText(messagemParabens.getDescricao());
+            emailSender.send(message);
+
+            return ResponseEntity.ok().build();
+        } catch (MailException e){
+            System.out.println(e.getMessage());
+            return ResponseEntity.badRequest().build();
+        }
+    }
+
+    private static String gerarSenha() {
         UUID uuid = UUID.randomUUID();
         String senha = uuid.toString().substring(0, 7);
 
         return senha;
     }
+
+
 }
